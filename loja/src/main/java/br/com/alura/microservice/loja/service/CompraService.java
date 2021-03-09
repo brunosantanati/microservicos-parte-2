9443dfd7b4dcc7c4305d66dcd3d75316cea50ddf
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-03-09 11:54:24 -0300

    Arquivos iniciais do curso.

---
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -0,0 +1,36 @@
+package br.com.alura.microservice.loja.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.model.Compra;
+
+@Service
+public class CompraService {
+	
+	@Autowired
+	private FornecedorClient fornecedorClient;
+
+	public Compra realizaCompra(CompraDTO compra) {
+		
+		final String estado = compra.getEndereco().getEstado();
+		
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
+		
+		InfoPedidoDTO infoPedido = fornecedorClient.realizaPedido(compra.getItens());
+		
+		Compra compraSalva = new Compra();
+		compraSalva.setPedidoId(infoPedido.getId());
+		compraSalva.setTempoDePreparo(infoPedido.getTempoDePreparo());
+		compraSalva.setEnderecoDestino(info.getEndereco());
+		
+		System.out.println(info.getEndereco());
+		
+		return compraSalva;
+	}
+	
+}
