c7ff54698f4b5c63736226598c85dda76daca807
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-05-10 16:55:38 -0300

    Registrar Zuul no Eureka e utilizar ip e porta do Zuul.

--- zuul/src/main/java/br/com/alura/microservice/zuul/ZuulApplication.java
+++ zuul/src/main/java/br/com/alura/microservice/zuul/ZuulApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.microservice.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
 
 	public static void main(String[] args) {
