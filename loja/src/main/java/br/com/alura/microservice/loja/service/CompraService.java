98a6383ae6cba1cb0ff461f77a0200949ee87a9d
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-05-10 14:18:22 -0300

    Comentario sobre reprocessamento e cancelamento.

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -35,6 +35,23 @@ public class CompraService {
 		return compraRepository.findById(id).orElse(new Compra());
 	}
 	
+	
+	/*
+	 * Comentário extraído da atividade na Alura, referente a reprocessamento e cancelamento:
+	 * 
+	 * Como vimos, a Loja possui duas responsabilidades: a de executar todos os passos necessários para que a compra seja realizada e o de manter os dados da compra. 
+	 * Quando é falado que o cliente deve se responsabilizar pela decisão do que fazer em caso de erro, seja para cancelar a compra ou reprocessar a mesma, 
+	 * quem seria esse cliente?
+	 * 
+	 * Uma outra funcionalidade que tenha a lógica de orquestração da transação de compra
+	 * Alternativa correta! O método realizaCompra sabe exatamente todos os passos necessários para que uma compra seja efetivada. 
+	 * O que precisamos além disso é quem saiba o que fazer quando esta compra não chega no estado final. 
+	 * Cancelar, reprocessar ou tentar mais n vezes? Atualmente, nós temos o CompraController chamando o CompraService.
+	 * Poderíamos criar mais um serviço e colocar entre o controller e o serviço de compra. 
+	 * Teríamos então o CompraController chamando o OrquestradorDeCompraService, que chamaria o CompraService. 
+	 * De acordo com o estado da compra retornada pelo CompraService, o orquestrador decidiria reprocessar, cancelar, etc.
+	 */
+	
 	/**
 	 * Caso a compra não seja processada corretamente, o cliente pode tentar reprocessa-la a partir do último estado da compra
 	 */
