0b8a683fbdccef13e638e7fc93edb3adf5696a5a
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-05-12 18:52:17 -0300

    Pedir autenticacao apenas para 'POST /pedido'.

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/ResourceServerConfigurer.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/ResourceServerConfigurer.java
@@ -1,6 +1,7 @@
 package br.com.alura.microservice.fornecedor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -10,8 +11,8 @@ public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.anyRequest()
-			.authenticated();
+			.antMatchers(HttpMethod.POST, "/pedido")
+			.hasRole("USER");
 	}
 	
 }
